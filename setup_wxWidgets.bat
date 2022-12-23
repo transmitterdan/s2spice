8d56954c798b26955188cb44b854e24fba8a7ede
transmitterdan <10525699+transmitterdan@users.noreply.github.com>
2022-12-23 13:54:11 -0600

    Update setup_wxWidgets.bat

--- setup_wxWidgets.bat
+++ setup_wxWidgets.bat
@@ -8,11 +8,11 @@ for /f "tokens=*" %%a in ('cd') do (
 )
 set "wxWIN=%VAR%\wxWidgets-%wxVER%"
 set "wxWidgets_ROOT_DIR=%wxWIN%"
-curl -L -o "wxWidgets-Dev-%wxVER%.7z" "https://github.com/wxWidgets/wxWidgets/releases/download/v%wxVER%/wxMSW-%wxVER%_vc14x_Dev.7z"
-7z x "wxWidgets-Dev-%wxVER%.7z" -o %WXWIN%
+curl --output "wxWidgets-Dev-%wxVER%.7z" "https://github.com/wxWidgets/wxWidgets/releases/download/v%wxVER%/wxMSW-%wxVER%_vc14x_Dev.7z"
+7z x "wxWidgets-Dev-%wxVER%.7z" -o%WXWIN%
 del "wxWidgets-Dev-%wxVER%.7z"
-curl -L -o "wxWidgets-headers.7z" "https://github.com/wxWidgets/wxWidgets/releases/download/v%wxVER%/wxWidgets-%wxVER%-headers.7z"
-7z x "wxWidgets-headers.7z" -o %WXWIN%
+curl --output "wxWidgets-headers.7z" "https://github.com/wxWidgets/wxWidgets/releases/download/v%wxVER%/wxWidgets-%wxVER%-headers.7z"
+7z x "wxWidgets-headers.7z" -o%WXWIN%
 del "wxWidgets-headers.7z"
 cd s2spice
 for /f "tokens=*" %%a in ('dir /b %wxWidgets_ROOT_DIR%\lib') do (
@@ -21,6 +21,7 @@ for /f "tokens=*" %%a in ('dir /b %wxWidgets_ROOT_DIR%\lib') do (
 set "WxWidgets_LIB_DIR=%wxWidgets_ROOT_DIR%\lib\%VAR%"
 set "Path=%wxWidgets_LIB_DIR%;%PATH%
 cd s2spice
+if exist .\build rmdir /s /q .\build
 mkdir build && cd build && cmake -A wIN32 .. && cmake --build . --config Release && cmake --build . --config Debug
 if %errorlevel% == 0 goto :ok
 @echo Error during build proces
